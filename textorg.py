d7ad2ef6a06624e17a173ab872b7970baff37119
Lin Chen <abratdarcy@gmail.com>
2022-01-31 18:23:46 -0800

    字频统计

--- textorg.py
+++ textorg.py
@@ -1,3 +1,4 @@
+from collections import Counter
 import PySimpleGUI as sg
 
 
@@ -13,11 +14,26 @@ def countChinese(text):
     return cnt
 
 
+def chineseStat(text):
+    cnt = Counter()
+    for ch in text:
+        if isChinese(ch):
+            cnt[ch] += 1
+    res = '\n'.join('%s：\t%d' % (ch, freq) 
+    for ch, freq in sorted(cnt.items(), key=lambda x:x[1], reverse=True))
+    sg.popup_scrolled(res, title='字频统计', font=('宋体', 15))
+
 def output(text):
     window['out'].update(text)
 
 
-layout = [ [sg.Button('字数统计') ],
+tab1_layout =  [[sg.Button('字数统计'),sg.Button('字频统计') ]]    
+
+tab2_layout = [[sg.T('This is inside tab 2')],    
+               ]
+
+layout = [ [sg.TabGroup([[sg.Tab('通用', tab1_layout, tooltip='tip'), sg.Tab('Tab 2', tab2_layout)]], 
+    tooltip='TIP2', expand_x=True)],
             [sg.Text('', key='out')],
             [sg.Multiline(key='main')]
         ]
@@ -32,5 +48,7 @@ while True:
         break
     elif event in ('字数统计'):
         output('汉字：%d' % (countChinese(values['main'])))
+    elif event in ('字频统计'):
+        chineseStat(values['main'])
 
 window.close()
